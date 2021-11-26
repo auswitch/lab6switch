862f4d246b3988bded190e723cd4663f123759fd
auswitch <sawit_cha@cmu.ac.th>
2021-11-26 16:25:47 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int z , even = 0 ,odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> z;
+    while (z != 0)
+    {
+        if (z%2 == 0){
+            even += 1;
+        }
+        else{
+            odd += 1;
+        }
+        cout << "Enter an integer: ";
+        cin >> z;
+    }
+    cout << "#Even numbers = " <<even <<endl;
+    cout << "#Odd numbers = " <<odd;
     return 0;
 }
